fd4a5b8a94fff9f7acb8e2852b13abc6f963e309
gayathribodagala <bodagala.gayathri2021@vitstudent.ac.in>
2024-01-07 21:03:43 +0530

    added HelloJava program

---
+++ HelloJava.java
@@ -0,0 +1,7 @@
+class HelloJava{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");
+     System.out.println("This is Gayathri");
+     System.out.println("21MIC0030");
+    }  
+}    
